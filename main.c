3831624e68d681895c7e0ff462f2a142ac916f70
Ryan Trinh <rqtrinh@cpp.edu>
2021-11-30 13:43:52 -0800

    Worked on launch and builtin function routines

--- main.c
+++ main.c
@@ -117,3 +117,89 @@ char **lsh_split_line(char *line)
     tokens[position] = NULL;
     return tokens;
 }
+
+int lsh_launch(char **args)
+{
+    pid_t pid, wpid;
+    int status
+
+    pid = fork();
+    if (pid == 0) {
+        //Child process
+        if(execvp(args[0], args) == -1) {
+            perror("lsh");
+        }
+        exit(EXIT_FAILURE);
+    } else if (pid < 0){
+        //Error forking
+        perror("lsh");
+    } else {
+        //Parent process
+        do {
+            wpid = waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+
+    return 1;
+}
+
+/* 
+    Function Declarations for bultin shell commands:
+*/
+int lsh_cd(char **args);
+int lsh_help(char **args);
+int lsh_exit(char **args);
+
+/* 
+    List of builtin commands, followed by their corresponding fucntions.
+*/
+char *builtin_str[] = {
+    "cd",
+    "help",
+    "exit"
+};
+
+int (*builtin_func[]) (char **) = {
+    &lsh_cd,
+    &lsh_help,
+    lsh_exit
+};
+
+int lsh_num_builtins() {
+    return sizeof(builtin_str) / sizeof(char *);
+}
+
+/* 
+    Builtin function implementations.
+*/
+int lsh_cd(char **args)
+{
+    if (args[1] == NULL) {
+        fprintf(stderr, "lsh: expected argument to \"cd\"\n");
+    } else {
+        if (chdir(args[1]) != 0) {
+            perror("lsh");
+        }
+    }
+    return 1;
+}
+
+int lsh_help(char **args)
+{
+    int i;
+    printf("Ryan Trinh's LSH\n");
+    printf("Type program names and arguements, and hit enter.\n");
+    printf("The following are built in:\n");
+
+    for (i = 0; i < lsh_num_builtins(); i++){
+        printf("  %s\n", builtin_str[i]);
+    }
+
+    printf("Use the man command for information on other programs. \n");
+    return 1
+}
+
+int lsh_exit(char **args)
+{
+    return 0;
+}
