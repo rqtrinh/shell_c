28733fb3e846b90bde4ba8e2aa8a28a90efd4dce
Ryan Trinh <rqtrinh@cpp.edu>
2021-11-30 16:48:10 -0800

    Reorganized code, commented out less optimal lsh_read_line, fixed some syntax errors

--- main.c
+++ main.c
@@ -4,38 +4,113 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char **argv)
-{
-    //Load config files, if any.
 
-    //Run command loop.
-    lsh_loop();
+/* 
+    Function Declarations for bultin shell commands:
+*/
+int lsh_cd(char **args);
+int lsh_help(char **args);
+int lsh_exit(char **args);
 
-    //Preform any shutdown/cleanup.
+/* 
+    List of builtin commands, followed by their corresponding fucntions.
+*/
+char *builtin_str[] = {
+    "cd",
+    "help",
+    "exit"
+};
 
-    return EXIT_SUCCESS;
+int (*builtin_func[]) (char **) = {
+    &lsh_cd,
+    &lsh_help,
+    &lsh_exit
+};
+
+int lsh_num_builtins() {
+    return sizeof(builtin_str) / sizeof(char *);
 }
 
-void lsh_loop(void)
+/* 
+    Builtin function implementations.
+*/
+int lsh_cd(char **args)
 {
-    char *line;
-    char **args;
+    if (args[1] == NULL) {
+        fprintf(stderr, "lsh: expected argument to \"cd\"\n");
+    } else {
+        if (chdir(args[1]) != 0) {
+            perror("lsh");
+        }
+    }
+    return 1;
+}
+
+int lsh_help(char **args)
+{
+    int i;
+    printf("Ryan Trinh's LSH\n");
+    printf("Type program names and arguements, and hit enter.\n");
+    printf("The following are built in:\n");
+
+    for (i = 0; i < lsh_num_builtins(); i++){
+        printf("  %s\n", builtin_str[i]);
+    }
+
+    printf("Use the man command for information on other programs. \n");
+    return 1;
+}
+
+int lsh_exit(char **args)
+{
+    return 0;
+}
+
+int lsh_launch(char **args)
+{
+    pid_t pid, wpid;
     int status;
 
-    do{
-        printf("> ");
-        //read command
-        line = lsh_read_line();
-        //parse command
-        args = lsh_split_line(line);
-        //execute command
-        status = lsh_execute(args);
+    pid = fork();
+    if (pid == 0) {
+        //Child process
+        if(execvp(args[0], args) == -1) {
+            perror("lsh");
+        }
+        exit(EXIT_FAILURE);
+    } else if (pid < 0){
+        //Error forking
+        perror("lsh");
+    } else {
+        //Parent process
+        do {
+            wpid = waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
 
-        free(line);
-        free(args);
-    }while (status);
+    return 1;
 }
 
+int lsh_execute(char **args)
+{
+    int i;
+
+    if (args[0] == NULL) {
+        //An empty command was entered.
+        return 1;
+    }
+
+    for (i = 0; i < lsh_num_builtins(); i++) {
+        if(strcmp(args[0], builtin_str[i]) == 0) {
+            return(*builtin_func[i])(args);
+        }
+    }
+
+    return lsh_launch(args);
+}
+
+//Less efficient version of lsh_read_line
+/*
 #define LSH_RL_BUFSIZE 1024
 char *lsh_read_line(void)
 {
@@ -73,7 +148,8 @@ char *lsh_read_line(void)
         }
     }
 }
-
+*/
+#define LSH_RL_BUFSIZE 1024
 char *lsh_read_line(void)
 {
     char *line = NULL;
@@ -124,106 +200,34 @@ char **lsh_split_line(char *line)
     return tokens;
 }
 
-int lsh_launch(char **args)
+void lsh_loop(void)
 {
-    pid_t pid, wpid;
-    int status
+    char *line;
+    char **args;
+    int status;
 
-    pid = fork();
-    if (pid == 0) {
-        //Child process
-        if(execvp(args[0], args) == -1) {
-            perror("lsh");
-        }
-        exit(EXIT_FAILURE);
-    } else if (pid < 0){
-        //Error forking
-        perror("lsh");
-    } else {
-        //Parent process
-        do {
-            wpid = waitpid(pid, &status, WUNTRACED);
-        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-    }
+    do{
+        printf("> ");
+        //read command
+        line = lsh_read_line();
+        //parse command
+        args = lsh_split_line(line);
+        //execute command
+        status = lsh_execute(args);
 
-    return 1;
+        free(line);
+        free(args);
+    }while (status);
 }
 
-/* 
-    Function Declarations for bultin shell commands:
-*/
-int lsh_cd(char **args);
-int lsh_help(char **args);
-int lsh_exit(char **args);
-
-/* 
-    List of builtin commands, followed by their corresponding fucntions.
-*/
-char *builtin_str[] = {
-    "cd",
-    "help",
-    "exit"
-};
-
-int (*builtin_func[]) (char **) = {
-    &lsh_cd,
-    &lsh_help,
-    lsh_exit
-};
-
-int lsh_num_builtins() {
-    return sizeof(builtin_str) / sizeof(char *);
-}
-
-/* 
-    Builtin function implementations.
-*/
-int lsh_cd(char **args)
+int main(int argc, char **argv)
 {
-    if (args[1] == NULL) {
-        fprintf(stderr, "lsh: expected argument to \"cd\"\n");
-    } else {
-        if (chdir(args[1]) != 0) {
-            perror("lsh");
-        }
-    }
-    return 1;
-}
+    //Load config files, if any.
 
-int lsh_help(char **args)
-{
-    int i;
-    printf("Ryan Trinh's LSH\n");
-    printf("Type program names and arguements, and hit enter.\n");
-    printf("The following are built in:\n");
+    //Run command loop.
+    lsh_loop();
 
-    for (i = 0; i < lsh_num_builtins(); i++){
-        printf("  %s\n", builtin_str[i]);
-    }
+    //Preform any shutdown/cleanup.
 
-    printf("Use the man command for information on other programs. \n");
-    return 1
-}
-
-int lsh_exit(char **args)
-{
-    return 0;
-}
-
-int lsh_execute(char **args)
-{
-    itn i;
-
-    if (args[0] == NULL) {
-        //An empty command was entered.
-        return1
-    }
-
-    for (i = 0; i < lsh_num_builtins(); i++) {
-        if(strcmp(args[0]), builtin_str[i] == 0) {
-            return(*builtin_func[i])(args);
-        }
-    }
-
-    return lsh_launch(args);
+    return EXIT_SUCCESS;
 }
